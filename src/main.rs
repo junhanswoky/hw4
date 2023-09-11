271a3d7f58410d6f28faacd84ce560bc5770c186
junhanswoky <107308721+junhanswoky@users.noreply.github.com>
2023-09-10 19:40:25 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use std::cell::RefCell;
+use std::sync::Condvar;
 use async_std::task::spawn;
 use std::collections::VecDeque;
 use std::future::Future;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll, Wake, Waker};
+use std::task::{Context, Poll, Waker, Wake};
 use futures::future::BoxFuture;
-use async_channel::bounded;
 
-thread_local! {
-    static SIGNAL: Arc<Signal> = Arc::new(Signal::new());
-    static RUNNABLE: Mutex<VecDeque<Arc<Task>>> = Mutex::new(VecDeque::with_capacity(1024));
+struct Signal {
+    state: Mutex<State>,
+    cond: Condvar,
 }
 
 enum State {
@@ -17,19 +18,13 @@ enum State {
     Notified,
 }
 
-struct Signal {
-    state: Mutex<State>,
-    cond: std::sync::Condvar,
-}
-
 impl Signal {
     fn new() -> Signal {
-        Signal {
-            state: Mutex::new(State::Notified),
-            cond: std::sync::Condvar::new(),
+        Signal { 
+            state: Mutex::new(State::Notified), 
+            cond: Condvar::new() 
         }
     }
-
     fn wait(&self) {
         let mut state = self.state.lock().unwrap();
         match *state {
@@ -39,13 +34,12 @@ impl Signal {
             }
             State::Empty => {
                 *state = State::Waiting;
-                while *state == State::Waiting {
+                while let State::Waiting = *state {
                     state = self.cond.wait(state).unwrap();
                 }
             }
         }
     }
-
     fn notify(&self) {
         let mut state = self.state.lock().unwrap();
         match *state {
@@ -66,15 +60,16 @@ impl Wake for Signal {
 }
 
 struct Task {
-    future: Mutex<BoxFuture<'static, ()>>,
+    future: RefCell<BoxFuture<'static, ()>>,
     signal: Arc<Signal>,
 }
 
+unsafe impl Send for Task {}
+unsafe impl Sync for Task {}
+
 impl Wake for Task {
     fn wake(self: Arc<Self>) {
-        RUNNABLE.with(|runnable| {
-            runnable.lock().unwrap().push_back(self.clone());
-        });
+        RUNNABLE.with(|runnable| runnable.lock().unwrap().push_back(self.clone()));
         self.signal.notify();
     }
 }
@@ -89,28 +84,35 @@ impl Future for Demo {
     }
 }
 
+scoped_tls::scoped_thread_local!(static SIGNAL: Arc<Signal>);
+scoped_tls::scoped_thread_local!(static RUNNABLE: Mutex<VecDeque<Arc<Task>>>);
+
 fn block_on<F: Future>(future: F) -> F::Output {
-    let mut fut = std::pin::Pin::new(Box::pin(future));
+    let mut fut = std::pin::pin!(future);
     let signal = Arc::new(Signal::new());
     let waker = Waker::from(signal.clone());
-    let mut cx = Context::from_waker(&waker);
 
-    SIGNAL.with(|signal| {
-        RUNNABLE.with(|runnable| {
-            while let Poll::Pending = fut.as_mut().poll(&mut cx) {
+    let mut cx = Context::from_waker(&waker);
+    let runnable: Mutex<VecDeque<Arc<Task>>> = Mutex::new(VecDeque::with_capacity(1024));
+    SIGNAL.set(&signal, || {
+        RUNNABLE.set(&runnable, || {
+            loop {
+                if let Poll::Ready(output) = fut.as_mut().poll(&mut cx) {
+                    return output;
+                }
                 while let Some(task) = runnable.lock().unwrap().pop_front() {
                     let waker = Waker::from(task.clone());
                     let mut cx = Context::from_waker(&waker);
-                    let _ = task.future.lock().unwrap().as_mut().poll(&mut cx);
+                    let _ = task.future.borrow_mut().as_mut().poll(&mut cx);
                 }
                 signal.wait();
             }
-        });
+        })
     })
 }
 
 async fn demo() {
-    let (tx, rx) = bounded(1);
+    let (tx, rx) = async_channel::bounded(1);
     spawn(demo2(tx));
     println!("hello world!");
     let _ = rx.recv().await;
